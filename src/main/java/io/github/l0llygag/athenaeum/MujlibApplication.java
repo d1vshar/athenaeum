5254f28eddbe8f184c326620ca5a5fa595b4398b
Divyanshu Sharma <32746859+l0llygag@users.noreply.github.com>
2020-01-11 23:19:55 +0530

    Intializr Init

---
+++ src/main/java/io/github/l0llygag/athenaeum/MujlibApplication.java
@@ -0,0 +1,13 @@
+package io.github.l0llygag.athenaeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujlibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MujlibApplication.class, args);
+    }
+
+}
