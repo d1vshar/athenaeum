ca5d1789b3b634b1387f96d961e7d620116181fa
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2020-01-24 23:26:05 +0530

    Added Book Schema Added create book endpoint

--- server/database/models/libitem.js
+++ server/database/models/libitem.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
 
 var LibraryItemSchema = new mongoose.Schema({
-    id: { type: Number },
+    bookId: { type: String },
     type: { type: String, required: true },
     name: { type: String, required: true },
-    isbn: { type: String, required: true },
-    author: { type: Array, required: true },
-    subject: { type: String, default: '' },
+    isbn10: { type: String, required: true },
+    isbn13: { type: String, required: true },
+    authors: { type: Array, required: true },
+    publisher: { type: String, required: true },
+    edition: { type: String },
+    description: { type: String },
+    subject: { type: String, default: 'Unknown' },
     amount: { type: Number, default: 0 },
-    location: { type: String, default: '' }
+    location: { type: String, defaultS: '' }
 })
 
 module.exports = LibraryItemSchema;
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET all books */
-router.get('/', function(req, res, next) {
+router.get('/books/', function (req, res, next) {
   const LibraryItemModel = mongoose.model('libitem');
-  LibraryItemModel.find().then( (items) => {
+  LibraryItemModel.find().then((items) => {
     return res.status(200).json(items);
   })
+});
+
+/* INSERT book */
+router.post('/books/', function (req, res, next) {
+  const LibraryItemModel = mongoose.model('libitem');
+  
+  var newBook = new LibraryItemModel({
+    bookId: req.body.bookId,
+    type: req.body.type,
+    name: req.body.name,
+    isbn10: req.body.isbn10,
+    isbn13: req.body.isbn13,
+    authors: req.body.authors,
+    publisher: req.body.publisher,
+    edition: req.body.edition,
+    description: req.body.description,
+    subject: req.body.subject,
+    amount: req.body.amount,
+    location: req.body.location
+  });
+
+  newBook.save();
+
+  return res.status(201).end();
 });
 
 module.exports = router;
