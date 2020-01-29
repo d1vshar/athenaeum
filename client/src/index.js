9550725676307fb1c60a51000428e1b8e0ed437b
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2020-01-30 00:21:09 +0530

    fix imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RootPage from './RootPage.js';
+import RootPage from './widgets/RootPage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<RootPage />, document.getElementById('root'));
