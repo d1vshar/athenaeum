ae8731e892a9fec79325f951f63d02bd3e220aa6
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2020-05-31 17:35:31 +0530

    Improved project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import RootPage from './components/RootPage';
-import * as serviceWorker from './serviceWorker';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import "../App.css"
+import "./App.css"
 
 const darkTheme = createMuiTheme({
     palette: {
@@ -15,12 +13,15 @@ const darkTheme = createMuiTheme({
     },
   });
 
-ReactDOM.render(
-<ThemeProvider theme={darkTheme} ><CssBaseline /><RootPage/></ThemeProvider>, 
-document.getElementById('root')
-);
+class App extends React.Component {
+  render() {
+    return (
+    <ThemeProvider theme={darkTheme} >
+      <CssBaseline />
+      <RootPage/>
+      </ThemeProvider>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default App
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
-import RootPage from './components/RootPage';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import "./App.css"
+import App from './App';
 
-const darkTheme = createMuiTheme({
-    palette: {
-      type: 'dark',
-      primary: { main: '#212121' },
-      secondary: { main: '#2121a1' }
-    },
-  });
-
-ReactDOM.render(
-<ThemeProvider theme={darkTheme} ><CssBaseline /><RootPage/></ThemeProvider>, 
-document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
